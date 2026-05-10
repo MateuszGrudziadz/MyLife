a7dac9bd4a90643d41ea9de0b4a52a9f38e9c187
MateuszGrudziadz <mateusz.grudziadz1706@gmail.com>
2026-05-10 14:16:57 +0200

    Dodanie template

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from core.templates import templates
 
-router = APIRouter(prefix="/users")
+router = APIRouter(prefix="/users", tags=["users"])
 
 @router.get("/")
+async def hello(request: Request):
+    return templates.TemplateResponse("hello.html", {"request": request, "hello": "Witaj na mojej stronie!"})
+
+@router.get("/all")
 def get_users():
     return {"users": ["siema"]}
